c31c54c385377e9770ee0f03235aa91cbf5f370e
MendFeld <53190630+MendFeld@users.noreply.github.com>
2019-07-24 14:50:32 -0700

    Add files via upload

---
+++ Popup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+
+namespace NotificationPopup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            PopupNotifier popup = new PopupNotifier();
+            popup.Image = Properties.Resources.info;
+            popup.TitleText = "Blss Up";
+            popup.ContentText = "Keep blessing the world !";
+            popup.Popup();//Show
+        }
+    }
+}
